985aab1507332c2a3f83242c6b1d2f797fe3c760
Morgován Nimród <morgovannimrod22@gmail.com>
2022-11-08 12:46:23 +0100

    Alap oldal felépítésének folytatása

--- src/Main.js
+++ src/Main.js
@@ -1,3 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 let bookDatabase = [];
+function ujraszamol() {
+    let Konyvekdb = document.getElementById('KonyvekDB');
+    let IngyenesKonyvekDB = document.getElementById('IngyenesKonyvekDB');
+    let KonyvekOsszAr = document.getElementById('KonyvekOsszAr');
+    Konyvekdb.innerHTML = bookDatabase.length.toString();
+    let bookDatabase_filtered = bookDatabase.map(e => e.price == 0);
+    IngyenesKonyvekDB.innerHTML = bookDatabase_filtered.length.toString();
+    KonyvekOsszAr.innerHTML = bookDatabase.map(e => e.price).reduce((a, b) => a + b, 0).toString();
+}
+document.addEventListener('DOMContentLoaded', () => {
+});
--- src/Main.ts
+++ src/Main.ts
@@ -1,4 +1,19 @@
 import { Book } from "./Book";
 import { EBook } from "./Ebook";
 import { PaperBook } from "./PaperBook";
-let bookDatabase = [];
+let bookDatabase: Book[] = [];
+
+function ujraszamol() {
+   let Konyvekdb = document.getElementById('KonyvekDB') as HTMLSpanElement;
+   let IngyenesKonyvekDB = document.getElementById('IngyenesKonyvekDB') as HTMLSpanElement;
+   let KonyvekOsszAr = document.getElementById('KonyvekOsszAr') as HTMLSpanElement;
+   Konyvekdb.innerHTML = bookDatabase.length.toString();
+   let bookDatabase_filtered = bookDatabase.map(e=>e.price == 0);
+   IngyenesKonyvekDB.innerHTML = bookDatabase_filtered.length.toString();
+   KonyvekOsszAr.innerHTML = bookDatabase.map(e=>e.price).reduce((a, b) => a + b, 0).toString();
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+});
